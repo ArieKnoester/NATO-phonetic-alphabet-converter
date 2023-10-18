a4004b89a6502b563f51db2988e92a38016b1076
Arie Knoester <arikstr@gmail.com>
2023-10-18 14:56:53 -0500

    Added exception handling for non-alpha characters.

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ nato_alphabet_df = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_alphabet_dict = {row.letter: row.code for index, row in nato_alphabet_df.iterrows()}
 
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_input = input("Enter a word: ")
-output = [nato_alphabet_dict.get(char.upper()) for char in user_input]
-print(output)
+# TODO 3. Add exception handling for non-alpha characters
+while True:
+    user_input = input("Enter a word: ")
+    try:
+        output = [nato_alphabet_dict[letter.upper()] for letter in user_input]
+    except KeyError:
+        print("Sorry, only characters in the alphabet are accepted.")
+    else:
+        print(output)
+        break
